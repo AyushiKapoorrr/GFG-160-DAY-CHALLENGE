357118c29818136050bb5314c393073c2ae55d81
Ayushi Kapoor <ayushii.kapoorrr@gmail.com>
2025-05-10 19:31:41 +0530

    Update day8.c

--- day8.c
+++ day8.c
@@ -1,1 +1,18 @@
-day 8
+// User function Template for Java
+
+class Solution {
+    public int maximumProfit(int prices[]) {
+        int minprice =Integer.MAX_VALUE;
+        int maxprofit =0;
+        
+        for(int price:prices){
+            if(price<minprice){
+                minprice=price;}
+            else if(price - minprice>maxprofit){
+                maxprofit=price-minprice;
+            }
+            }
+            return maxprofit;
+        }
+        
+    }
